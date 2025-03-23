7e090944b6724b79e24bf26d9b04c3dfc76861a1
Harini <93416184+Harini1905@users.noreply.github.com>
2025-03-23 14:25:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
-function showSuccessMessage() {
-    document.body.innerHTML = `
-        <h1>Yay! You're the best!</h1>
-        <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZnRtcDNsamluZWw2dmV1aHQwbTNhdThwbTN1ZGdibTR5NGxhaGdxbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/9Y1LEFKsbbP4hrLgV3/giphy.gif" 
-             alt="Happy Dance" 
-             style="width: 400px;">
-    `;
-}
-
-function swapButtons() {
-    let yesButton = document.querySelector('.yes');
-    let noButton = document.querySelector('.no');
-    
-    // Swap button positions
-    yesButton.classList.toggle('yes');
-    yesButton.classList.toggle('no');
-    noButton.classList.toggle('no');
-    noButton.classList.toggle('yes');
-    
-    // Swap text content
-    let tempText = yesButton.innerText;
-    yesButton.innerText = noButton.innerText;
-    noButton.innerText = tempText;
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    let buttonsContainer = document.querySelector(".buttons");
-    
-    buttonsContainer.addEventListener("mouseover", function(event) {
-        if (event.target.classList.contains("no")) {
-            swapButtons();
+ function showSuccessMessage() {
+            document.body.innerHTML = `
+                <h1>Yay! You're the best!</h1>
+                <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZnRtcDNsamluZWw2dmV1aHQwbTNhdThwbTN1ZGdibTR5NGxhaGdxbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/9Y1LEFKsbbP4hrLgV3/giphy.gif" alt="Happy Dance" style="width: 400px;">
+            `;
         }
-    });
-    
-    document.querySelector(".yes").addEventListener("click", showSuccessMessage);
-});
+        
+        function swapButtons() {
+            let yesButton = document.querySelector('.yes');
+            let noButton = document.querySelector('.no');
+            
+            // Swap button positions
+            yesButton.classList.toggle('yes');
+            yesButton.classList.toggle('no');
+            noButton.classList.toggle('no');
+            noButton.classList.toggle('yes');
+            
+            // Swap text content
+            let tempText = yesButton.innerText;
+            yesButton.innerText = noButton.innerText;
+            noButton.innerText = tempText;
+        }
+        
+        document.addEventListener("DOMContentLoaded", function() {
+            let buttonsContainer = document.querySelector(".buttons");
+            
+            buttonsContainer.addEventListener("mouseover", function(event) {
+                if (event.target.classList.contains("no")) {
+                    swapButtons();
+                }
+            });
+            
+            document.querySelector(".yes").addEventListener("click", showSuccessMessage);
+        });
