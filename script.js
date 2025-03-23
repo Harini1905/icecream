d7de46fb69897043f68f143a175a2d7233275b66
Harini <93416184+Harini1905@users.noreply.github.com>
2025-03-23 17:10:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
- function showSuccessMessage() {
-            document.body.innerHTML = `
-                <h1>Yay! You're the best!</h1>
-                <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZnRtcDNsamluZWw2dmV1aHQwbTNhdThwbTN1ZGdibTR5NGxhaGdxbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/9Y1LEFKsbbP4hrLgV3/giphy.gif" alt="Happy Dance" style="width: 400px;">
-            `;
+function showSuccessMessage() {
+    document.body.innerHTML = `
+        <h1>Yay! You're the best!</h1>
+        <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZnRtcDNsamluZWw2dmV1aHQwbTNhdThwbTN1ZGdibTR5NGxhaGdxbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/9Y1LEFKsbbP4hrLgV3/giphy.gif" alt="Happy Dance" style="width: 400px;">
+    `;
+}
+
+function swapButtons() {
+    let yesButton = document.querySelector('.yes');
+    let noButton = document.querySelector('.no');
+    
+    // Swap button positions
+    yesButton.classList.toggle('yes');
+    yesButton.classList.toggle('no');
+    noButton.classList.toggle('no');
+    noButton.classList.toggle('yes');
+    
+    // Swap text content
+    let tempText = yesButton.innerText;
+    yesButton.innerText = noButton.innerText;
+    noButton.innerText = tempText;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttonsContainer = document.querySelector(".buttons");
+
+    buttonsContainer.addEventListener("mouseover", function(event) {
+        if (event.target.classList.contains("no")) {
+            swapButtons();
         }
-        
-        function swapButtons() {
-            let yesButton = document.querySelector('.yes');
-            let noButton = document.querySelector('.no');
-            
-            // Swap button positions
-            yesButton.classList.toggle('yes');
-            yesButton.classList.toggle('no');
-            noButton.classList.toggle('no');
-            noButton.classList.toggle('yes');
-            
-            // Swap text content
-            let tempText = yesButton.innerText;
-            yesButton.innerText = noButton.innerText;
-            noButton.innerText = tempText;
+    });
+
+    buttonsContainer.addEventListener("click", function(event) {
+        if (event.target.tagName === "BUTTON") {
+            showSuccessMessage();
         }
-        
-        document.addEventListener("DOMContentLoaded", function() {
-            let buttonsContainer = document.querySelector(".buttons");
-            
-            buttonsContainer.addEventListener("mouseover", function(event) {
-                if (event.target.classList.contains("no")) {
-                    swapButtons();
-                }
-            });
-            
-            buttonsContainer.addEventListener("click", function(event) {
-                if (event.target.tagName === "BUTTON") {
-                    showSuccessMessage();
-                }
-            });
-        });
+    });
+});
